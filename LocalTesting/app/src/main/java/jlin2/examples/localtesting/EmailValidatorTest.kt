94bd484423395cc502097a7654fbe25637f5b1a0
Junni2704 <133447019+Junni2704@users.noreply.github.com>
2024-08-01 21:33:20 -0400

    My Work for 2 correct emails and one false.

---
+++ LocalTesting/app/src/main/java/jlin2/examples/localtesting/EmailValidatorTest.kt
@@ -0,0 +1,29 @@
+package jlin2.examples.localtesting
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * Unit test class for EmailValidator.
+ */
+class EmailValidatorTest {
+
+    @Test
+    fun emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("123@abc.com"))
+    }
+
+    @Test
+    fun emailValidator_CorrectEmailSubDomain_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("123@abc.co.ca"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailNoTld_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("123@abc"))
+    }
+
+
+
+}
