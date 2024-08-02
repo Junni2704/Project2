d23480e6ece6abb6c85ff007a911d5ecde6c687a
Junni2704 <133447019+Junni2704@users.noreply.github.com>
2024-08-01 21:48:38 -0400

    Empty Input

---
+++ BasicSample/app/src/main/java/com/example/android/testing/espresso/BasicSample/MainActivityTest.java
@@ -0,0 +1,54 @@
+package com.example.android.testing.espresso.BasicSample;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.espresso.intent.Intents;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() {
+        Intents.init();
+    }
+
+    @After
+    public void tearDown() {
+        Intents.release();
+    }
+
+    @Test
+    public void testChangeTextButton_EmptyInput() {
+        onView(withId(R.id.changeTextBt)).perform(click());
+        onView(withId(R.id.textToBeChanged)).check(matches(withText("")));
+    }
+
+    @Test
+    public void testOpenActivityButton_EmptyInput() {
+        onView(withId(R.id.activityChangeTextBtn)).perform(click());
+        intended(hasComponent(ShowTextActivity.class.getName()));
+        intended(hasExtra(ShowTextActivity.KEY_EXTRA_MESSAGE, ""));
+    }
+    
+}
---
+++ BasicSample/app/src/main/java/com/example/android/testing/espresso/BasicSample/ShowTextActivityTest.java
@@ -0,0 +1,26 @@
+package com.example.android.testing.espresso.BasicSample;
+
+import android.content.Intent;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class ShowTextActivityTest {
+
+    @Test
+    public void testShowTextActivity_EmptyInput() {
+        Intent intent = ShowTextActivity.newStartIntent(ApplicationProvider.getApplicationContext(), "");
+        ActivityScenario.launch(intent);
+        onView(withId(R.id.show_text_view)).check(matches(withText("")));
+    }
+
+}
